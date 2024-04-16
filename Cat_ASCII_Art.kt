bb125ae1942d340fc380d7da7a46ef53ee0e8900
ZomB37 <HowlingNights@gmail.com>
2024-04-15 19:54:42 -0700

    Create Cat_ASCII_Art.kt

---
+++ Cat_ASCII_Art.kt
@@ -0,0 +1,32 @@
+// Write your code below
+// CodeCademy - Kotlin Course
+// ASCII Art Program
+// 4.15.24
+// SK
+//
+/*   
+         _____
+       / >      >
+       |  _   _ | 
+        .=__x =/
+      /         |
+    /   \       )
+  __|     |   |  |
+/ __|     |   |  |
+| ( \______\__)__)
+\___)
+
+*/
+
+fun main(){
+  println("          _____")
+  println("        / >      >")
+  println("        |  _   _ | ")
+  println("         .=__x =/")
+  println("       /         |")
+  println("     /   \\       )")
+  println("  __|     |   |  |")
+  println("/ __|     |   |  |")
+  println("| (  \\_____\\__)__)")
+  println("\\__)")
+}
